1c7a84e587619121e5f4a9366b3c6399dfeb5bd5
Thamires Oliveira <79226089+thaaoliveira1@users.noreply.github.com>
2023-03-28 22:03:32 +0200

    Fixing import codes

--- Code.py
+++ Code.py
@@ -1,16 +1,13 @@
-# utilitaries
+# utilities
 import re
 import numpy as np
 import pandas as pd
 # plotting
- import seaborn as sns
- from wordcloud import WordCloud
-
-import matplotlib.pyplot as 
-
+import seaborn as sns
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
 # nltk
 from nltk.stem import WordNetLemmatizer
-
 # sklearn
 from sklearn.svm import LinearSVC
 from sklearn.naive_bayes import BernoulliNB
@@ -18,3 +15,4 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import confusion_matrix, classification_report
+
