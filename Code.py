c7e1bd6b1b0f74c71e019d63f4a88fc99803e8fc
Thamires Oliveira <79226089+thaaoliveira1@users.noreply.github.com>
2023-03-28 10:59:17 +0200

    Added code file

---
+++ Code.py
@@ -0,0 +1,17 @@
+# utilitaries
+import re
+import numpy as np
+import pandas as pd
+# plotting
+import seaborn as sns
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+# nltk
+from nltk.stem import WordNetLemmatizer
+# sklearn
+from sklearn.svm import LinearSVC
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import confusion_matrix, classification_report
