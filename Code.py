dbe863a88308af2748acbb814ec520ab6fbec642
BitaDavoodi <davoodi.u@gmail.com>
2023-03-30 11:23:58 +0200

    Updated git ignore

--- Code.py
+++ Code.py
@@ -16,3 +16,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import confusion_matrix, classification_report
 
+
+
+
